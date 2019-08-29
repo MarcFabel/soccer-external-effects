a67111153054a5298b7b0333bedec1e3bdf76341
Marc Fabel <fabelm@g.cofc.edu>
2019-08-29 22:26:35 +0200

    combine csvs from leagues and append

--- analysis/prog/prepare_soccer_data.py
+++ analysis/prog/prepare_soccer_data.py
@@ -9,7 +9,11 @@ Created on Thu Aug 29 10:59:13 2019
 import pandas as pd
 
 #paths & magic numbers
-soccer_data = 'C:/Users/fabel/Dropbox/soc_ext_Dx/analysis/data/soccer/webscraping/output/'
+#soccer_data = 'C:/Users/fabel/Dropbox/soc_ext_Dx/analysis/data/soccer/webscraping/output/'
+
+
+# HOME directories 
+soccer_data = '/Users/marcfabel/Dropbox/soc_ext_Dx/analysis/data/soccer/webscraping/output/'
 
 #magic numbers
 first_year_wave = 2010
@@ -70,6 +74,11 @@ l2.rename(columns={'               home_result_end':'home_result_end',
                    '               stadium':'stadium',
                    '               goal_order':'goal_order',
                    '               away_yellow':'away_yellow',}, inplace=True)
+    
+l3.rename(columns={'               home_result_end':'home_result_end',
+                   '               stadium':'stadium',
+                   '               goal_order':'goal_order',
+                   '               away_yellow':'away_yellow',}, inplace=True)
 
 
 
@@ -83,9 +92,59 @@ l1.loc[game_identifier, 'away_result_break'] = 1
 
 
 
-
 # append the different leagues
 matches = l1.append(l2)
+matches = matches.append(l3)
+
+
+# drop some variables
+matches.drop(['away_ball', 'home_ball', 'goal_own', 'own_goals', 'grade_game', 
+              'home_chances', 'away_chances', 'home_corners', 'away_corners',
+              'home_shots', 'away_shots', 'sold_out', 'index'],
+    axis=1, inplace=True)
+matches.reset_index(inplace=True, drop=True)
+
+
+# VARIABLES THAT COULD BE REACTIVATED: chances (2x), corners (2x), sold_out
+
+
+# ordering of the variables
+#cols = list(matches.columns.values)
+matches = matches[[
+        'league',
+        'season',
+        'gameday',
+        'date',
+        'weekday',
+        'time',
+        'home_team',
+        'away_team',
+        'stadium',
+        'attendance',
+        'home_result_end',
+        'away_result_end',
+        'home_result_break',
+        'away_result_break',
+        'goal_order',
+        'goal_time',
+        'goal_penalty',
+        'penalties',
+        'away_red',
+        'away_yellow',
+        'home_red',
+        'home_yellow',
+        'grade_ref',
+        'ref_city',
+        'ref_name',
+        'ref_str'
+        ]]
+
+
+
+
+
+# what is with sold_out
+
 
 
 # Still missing data
