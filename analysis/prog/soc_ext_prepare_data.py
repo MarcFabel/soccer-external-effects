584b347faa1e4b43448658d7d1da63c5855f6c5c
Unknown <fabelm@g.cofc.edu>
2019-10-07 16:08:45 +0200

    merge regional_data

--- analysis/prog/soc_ext_prepare_data.py
+++ analysis/prog/soc_ext_prepare_data.py
@@ -25,8 +25,9 @@ import numpy as np
 
 # paths
 z_holidays =            'F:/econ/soc_ext/analysis/data/final/holidays/'
-z_crime_intermed =      'F:/econ/soc_ext/analysis/data/intermediate/crime/' 
+z_crime_intermed =      'F:/econ/soc_ext/analysis/data/intermediate/crime/'
 z_weather =             'F:/econ/soc_ext/analysis/data/intermediate/weather/'
+z_regional_data =       'F:/econ/soc_ext/analysis/data/intermediate/regional_database/'
 z_prefix =              'soc_ext_'
 
 
@@ -58,10 +59,9 @@ holidays.rename(columns={'mdy':'date'}, inplace=True)
 ########## WEATHER ##########
 weather = pd.read_csv(z_weather + 'weather_prepared.csv', sep=';', encoding='UTF-8')
 # cut weather to the right time window
-weather['date'] = pd.to_datetime(weather['date'], format='%Y-%m-%d')         
+weather['date'] = pd.to_datetime(weather['date'], format='%Y-%m-%d')
 weather.drop( weather[  (weather.date < start) | (weather.date > end)].index, inplace=True)
 weather.drop(['Unnamed: 0'], inplace=True, axis=1)
-
 
 
 ########## CRIME ##########
@@ -71,30 +71,41 @@ assaults.rename(columns={'date_d_mod':'date', 'location':'AGS'}, inplace=True)
 assaults['date'] = pd.to_datetime(assaults['date'], format='%Y-%m-%d')
 
 
-
-
+########## REGIONAL DATA BASE ##########
+regional_data = pd.read_csv(z_regional_data + 'regional_data_prepared.csv', sep=';', encoding='UTF-8')
 
 
 ###############################################################################
 #       2) Combine all data frames
 ###############################################################################
 
+
 # generate data frame with 96878 TxR rows (T=365+366+365+365+181=1642)  X  (R=59)
 data = weather.copy()
 data['bula'] = data.AGS.apply(lambda x: x/1000000).astype(int)
+# generate other variables from the date variable
+data['day'] 	= data.date.apply(lambda x: x.strftime('%d')).astype(int)
+data['month'] 	= data.date.apply(lambda x: x.strftime('%m')).astype(int)
+data['year'] 	= data.date.apply(lambda x: x.strftime('%Y')).astype(int)
+data['dow'] 	= data.date.apply(lambda x: x.strftime('%a'))
+data['doy'] 	= data.date.apply(lambda x: x.strftime('%j')).astype(int)
+data['woy'] 	= data.date.apply(lambda x: x.strftime('%W')).astype(int)
 
+
+# merge other data frames to it
 data = data.merge(holidays, on=['bula', 'date'], how='outer')
 data = data.merge(assaults, on=['date', 'AGS'], how='outer')
 data.offences.fillna(0, inplace=True)
+data = data.merge(regional_data, on=['AGS', 'year'])
 
-# merge soccer, merge regional_variables
-
-
-
+# merge soccer
 
 
 
 
+
+
+# generate offense rates
 
 
 
@@ -110,6 +121,7 @@ data.offences.fillna(0, inplace=True)
 
 
 # XXX TO DO:
+# regional_data: finish with generating the variables from other columns and drop irrelevnt ones, as they serve as input elsewhere
 # for a later time: kick weird stadiums out
 # ACTIVE REGIONS ARE NOT ACTIVE ALL THE TIME, IN SOME YEARS SOME TEAMS ARE NOT EVEN PART OF THE THIRD LEAGUE
 
