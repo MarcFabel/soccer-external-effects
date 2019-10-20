7ccfb1a803bb0ae02d594a0abc92f92b4a08535d
Unknown <fabelm@g.cofc.edu>
2019-10-20 15:19:59 +0200

    add: assault rates for subcategories (gender, relationship s-v, age, attempt)

--- analysis/prog/soc_ext_prepare_data.py
+++ analysis/prog/soc_ext_prepare_data.py
@@ -32,14 +32,20 @@ import time
 start_time = time.time()
 
 
-# paths
-z_prepared_data =             'C:/Users/fabel/Dropbox/soc_ext_Dx/analysis/data/final/'
-z_maps_input_intermediate =   'C:/Users/fabel/Dropbox/soc_ext_Dx/analysis/data/intermediate/maps/'
-z_data_output_Dx =            'C:/Users/fabel/Dropbox/soc_ext_Dx/analysis/data/final/'
-z_data_output =               'F:/econ/soc_ext/analysis/data/final/'
+# paths (work LOCAL)
+#z_prepared_data =             'C:/Users/fabel/Dropbox/soc_ext_Dx/analysis/data/final/'
+#z_maps_input_intermediate =   'C:/Users/fabel/Dropbox/soc_ext_Dx/analysis/data/intermediate/maps/'
+#z_data_output_Dx =            'C:/Users/fabel/Dropbox/soc_ext_Dx/analysis/data/final/'
+#z_data_output =               'F:/econ/soc_ext/analysis/data/final/'
+#z_prefix =                    'soc_ext_'
 
+
+# paths HOME
+z_prepared_data =             '/Users/marcfabel/Dropbox/soc_ext_Dx/analysis/data/final/'
+z_maps_input_intermediate =   '/Users/marcfabel/Dropbox/soc_ext_Dx/analysis/data/intermediate/maps/'
+z_data_output_Dx =            '/Users/marcfabel/Dropbox/soc_ext_Dx/analysis/data/final/'
 z_prefix =                    'soc_ext_'
-
+#z_data_output =              not applicable
 
 
 # magic numbers
@@ -79,12 +85,12 @@ soccer.drop( soccer[  (soccer.date < start) | (soccer.date > end)].index, inplac
 # two matches in same AGS on the same day
 double_matches = soccer[soccer.duplicated(subset=['date', 'AGS'], keep = False)]
 double_matches = double_matches[['date', 'home_team']]
-double_matches['D_multi_games'] = 1
+double_matches['d_multi_games'] = 1
 # drop smaller game league = 3 ( affects 6 games: Stuttgarter Kickers 2x, VfB 3x, and Fortuna Köln)
 soccer = soccer.merge(double_matches, on=['date', 'home_team'], how='outer')
-soccer['D_multi_games'].fillna(0, inplace=True)
-soccer.drop( soccer[ (soccer['D_multi_games'] == 1) & (soccer['league'] == 3)].index, inplace=True)
-soccer['D_gameday'] = 1
+soccer['d_multi_games'].fillna(0, inplace=True)
+soccer.drop( soccer[ (soccer['d_multi_games'] == 1) & (soccer['league'] == 3)].index, inplace=True)
+soccer['d_gameday'] = 1
 
 
 ########## WEATHER ##########
@@ -134,16 +140,15 @@ data['days_in_year'] = np.where(data.year == 2012, 366, 365)
 # merge other data frames to it
 data = data.merge(holidays, on=['bula', 'date'], how='outer')
 data = data.merge(assaults, on=['date', 'AGS'], how='outer')
-data.assaults.fillna(0, inplace=True)
+data.ass.fillna(0, inplace=True)
 data = data.merge(regional_data, on=['AGS', 'year'])
 
 # merge soccer
 data = data.merge(soccer, on=['AGS', 'date'], how='outer')
-# result should be 96878 x 296
-data['D_gameday'].fillna(0, inplace=True)
+data['d_gameday'].fillna(0, inplace=True)
 
 
-
+# up till here: dimension should be 96878 x 308
 
 
 
@@ -152,14 +157,44 @@ data['D_gameday'].fillna(0, inplace=True)
 #       Generate variables
 ###############################################################################
 
-# generate assault rates
-data['assault_rate'] = (data['assaults'] * 100000 * data['days_in_year'])/data['pop_t']
+# generate assault rate
+data['assrate'] = (data['ass'] * 100000 * data['days_in_year'])/data['pop_t']
+
+# assault rates per gender and age-group (denominator CASE specific)
+data['assrate_f'] = (data['ass_f'] * 100000 * data['days_in_year'])/data['pop_f']
+data['assrate_m'] = (data['ass_m'] * 100000 * data['days_in_year'])/data['pop_m']
+
+data['pop_0_17_t']  = data[['pop_0_2_t', 'pop_3_5_t', 'pop_6_9_t', 'pop_10_14_t', 'pop_15_17_t']].sum(axis='columns')
+data['pop_18_29_t'] = data[['pop_18_19_t','pop_20_24_t', 'pop_25_29_t']].sum(axis='columns')
+data['pop_30_39_t'] = data[['pop_30_34_t', 'pop_35_39_t']].sum(axis='columns')
+data['pop_40_49_t'] = data[['pop_40_44_t', 'pop_45_49_t']].sum(axis='columns')
+data['pop_50_59_t'] = data[['pop_50_54_t', 'pop_55_59_t']].sum(axis='columns')
+data['pop_60+_t']   = data[['pop_60_64_t', 'pop_65_74_t', 'pop_74+_t']].sum(axis='columns')
+
+data['assrate_0_17']  = (data['ass_0_17']  * 100000 * data['days_in_year'])/data['pop_0_17_t']
+data['assrate_18_29'] = (data['ass_18_29'] * 100000 * data['days_in_year'])/data['pop_18_29_t']
+data['assrate_30_39'] = (data['ass_30_39'] * 100000 * data['days_in_year'])/data['pop_30_39_t']
+data['assrate_40_49'] = (data['ass_40_49'] * 100000 * data['days_in_year'])/data['pop_40_49_t']
+data['assrate_50_59'] = (data['ass_50_59'] * 100000 * data['days_in_year'])/data['pop_50_59_t']
+data['assrate_60+']   = (data['ass_60+']   * 100000 * data['days_in_year'])/data['pop_60+_t']
+data.drop(['pop_0_17_t', 'pop_18_29_t', 'pop_30_39_t', 'pop_40_49_t', 'pop_50_59_t', 'pop_60+_t'],inplace=True, axis=1)
+
+# assault_rate: relationship and attempt/success (normal denominator)
+data['assrate_vs_strangers'] = (data['ass_vs_strangers'] * 100000 * data['days_in_year'])/data['pop_t']
+data['assrate_vs_rel'] = (data['ass_vs_rel'] * 100000 * data['days_in_year'])/data['pop_t']
+data['assrate_attempt'] = (data['ass_attempt'] * 100000 * data['days_in_year'])/data['pop_t']
+data['assrate_success'] = (data['ass_success'] * 100000 * data['days_in_year'])/data['pop_t']
 
 
+
+
+# result should be 96878 x 321
+assert(data.shape == (96878, 321))
 
 ###############################################################################
 #       Restrict sample
 ###############################################################################
+
 
 
 
@@ -168,7 +203,7 @@ data['assault_rate'] = (data['assaults'] * 100000 * data['days_in_year'])/data['
 ###############################################################################
 
 data.to_csv(z_data_output_Dx + 'data_prepared.csv', sep=';', encoding='UTF-8', index=False)
-data.to_csv(z_data_output + 'data_prepared.csv', sep=';', encoding='UTF-8', index=False)
+#data.to_csv(z_data_output + 'data_prepared.csv', sep=';', encoding='UTF-8', index=False)
 
 
 ###############################################################################
